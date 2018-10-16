0bde505b5970e5ec6e7400d6220e3ab585a4bdcf
somosoperationsreporting <44203536+somosoperationsreporting@users.noreply.github.com>
2018-10-16 15:48:12 -0400

    Update enddateformat.js

--- tableau-wdc-cloudwatch-master/scripts/enddateformat.js
+++ tableau-wdc-cloudwatch-master/scripts/enddateformat.js
@@ -2,8 +2,8 @@ const el = document.getElementById("endtime")
 
 var today = new Date();
 
-const year = today.getFullYear()
-const day = today.getDate()
+const year = today.getFullYear();
+const day = today.getDate();
 const month = today.getMonth() + 1;
 const minutes = today.getMinutes();
 const hour = today.getHours();
